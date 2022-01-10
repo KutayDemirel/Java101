b5d96eef4f89234b044dbcea59a62d36834311c2
KutayDemirel <kty.demirel@gmail.com>
2022-01-11 01:40:16 +0300

    Ilk deneme

---
+++ src/Deneme.java
@@ -0,0 +1,33 @@
+package Giris;
+import java.util.Scanner;
+
+public class Deneme {
+    public static void main(String[] args){
+        int mat, fizik, kimya, turkce, tarih, muzik;
+        Scanner input = new Scanner(System.in);
+        System.out.print("Matematik değerini giriniz:");
+        mat = input.nextInt();
+
+        System.out.print("Fizik notunu giriniz");
+        fizik = input.nextInt();
+
+        System.out.print("Kimya notunu giriniz");
+        kimya = input.nextInt();
+
+        System.out.print("Türkçe notunu giriniz:");
+        turkce = input.nextInt();
+
+        System.out.print("Tarih notunu giriniz");
+        tarih = input.nextInt();
+
+        System.out.print("Müzik notunu giriniz");
+        muzik = input.nextInt();
+
+        int toplam;
+        double ort;
+        toplam = (mat+fizik+kimya+turkce+tarih+muzik);
+        ort = toplam /6.0;
+        System.out.println("Derslerin ortalaması :   "+ort);
+
+    }
+}
