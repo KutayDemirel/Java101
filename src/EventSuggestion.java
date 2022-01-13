208d5888188a3793d6ad1fa424dd1ce967f34839
KutayDemirel <kty.demirel@gmail.com>
2022-01-13 03:15:39 +0300

    EventSuggestion

--- src/EventSuggestion.java
+++ src/EventSuggestion.java
@@ -1,2 +1,21 @@
-package PACKAGE_NAME;public class EventSuggestion {
+import java.util.Scanner;
+
+public class EventSuggestion {
+    public static void main(String[] args) {
+        int heat;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter Temperature value");
+        heat = input.nextInt();
+
+        if (heat < 5)
+            System.out.println("Our suggestion is Ski");
+        else if (heat < 15)
+            System.out.println("Our suggestion is Cinema");
+        if (heat >= 10 && heat < 25)
+            System.out.println("Our suggestion is Picnic");
+        else if (heat >= 25)
+            System.out.println("Our suggestion is Swimming");
+    }
 }
