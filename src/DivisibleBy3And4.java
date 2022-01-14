ef96df529f4b62b6fbee2ffefa33975187d38955
KutayDemirel <kty.demirel@gmail.com>
2022-01-14 15:52:21 +0300

    Divisible

---
+++ src/DivisibleBy3And4.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class DivisibleBy3And4 {
+    public static void main(String[] argh) {
+        int num;
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Please enter a number  :");
+        num = input.nextInt();
+        for (int i = 0; i <= num; i++) {
+            if (i % 3 == 0 || i % 4 == 0)
+                System.out.println(i);
+        }
+    }
+}
