5dd101d4037bd8d14f676615b45080a364fb354c
KutayDemirel <kty.demirel@gmail.com>
2022-01-16 23:38:43 +0300

    ReversedTriangle.2

--- BeginnerCodes/src/ReversedTriangle2.java
+++ BeginnerCodes/src/ReversedTriangle2.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class ReversedTringle2 {
+public class ReversedTriangle2 {
     public static void main(String[] args) {
         int digit;
         Scanner input = new Scanner(System.in);
