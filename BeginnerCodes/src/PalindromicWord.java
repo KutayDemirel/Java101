c2ac67a2dcec2d5dad8683ed8fb4caa2166f26fe
KutayDemirel <kty.demirel@gmail.com>
2022-01-28 17:09:15 +0300

    .\PalindromicWord.java

--- BeginnerCodes/src/PalindromicWord.java
+++ BeginnerCodes/src/PalindromicWord.java
@@ -11,12 +11,30 @@ public class PalindromicWord {
         return a.equals(temp);
     }
 
+    static boolean isControl(String str) {
+        int end = str.length() - 1, start = 0;
+        while (end > start) {
+            if (str.charAt(start) != str.charAt(end))
+                return false;
+            end--;
+            start++;
+        }
+        return true;
+    }
+
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         String word = input.nextLine();
 
+
         String s = isPalindromic(word) ? word + " is a palindromic word" : word + " is NOT palindromic word";
         System.out.println(s);
 
+        /*
+        if(!isControl(word))
+            System.out.println("no");
+        else
+            System.out.println("yes");
+         */
     }
 }
