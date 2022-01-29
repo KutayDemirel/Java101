4afa6ed3a9ed18ea7053b7e7dcb44ef2242159fb
KutayDemirel <kty.demirel@gmail.com>
2022-01-29 19:49:46 +0300

    .\MineSweeper.java

--- MineSweeper/src/Main.java
+++ MineSweeper/src/Main.java
@@ -1,27 +1,7 @@
-import java.util.Arrays;
-
 public class Main {
     public static void main(String[] args) {
-        MineSweeper mine = new MineSweeper(5,5);
-        String[][] asd ={{"kutay"}, {"yatay"}};
+        MineSweeper mine = new MineSweeper(2,2);
 
-        mine.MineBoard();
-        mine.print();
         mine.run();
-
-
-
-
-
-
-
-        /*int[] a = new int[5];
-
-        for (int i = 0 ; i < 5 ; i++)
-            a[i] = (int) (Math.random() * 10);
-
-        System.out.println(Arrays.toString(a));
-
-         */
     }
 }
--- MineSweeper/src/MineSweeper.java
+++ MineSweeper/src/MineSweeper.java
@@ -1,4 +1,3 @@
-import java.util.Objects;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -6,90 +5,136 @@ public class MineSweeper {
     Scanner input = new Scanner(System.in);
     int row;
     int col;
-    int satir;
-    int kolon;
-    String[][] board2;
-    String[][] played;
+    int selectedRow;
+    int selectedCol;
+    String[][] mineBoard;
+    String[][] gameBoard;
+    boolean isMine = false;
 
     MineSweeper(int row, int col) {
 
-        int[][] board = new int[row][col];
-        this.board2 = new String[row][col];
+        this.gameBoard = new String[row][col];
+        this.mineBoard = new String[row][col];
         this.row = row;
         this.col = col;
     }
 
-    int run() {
-        System.out.println("Satır giriniz  :");
-        this.satir = input.nextInt();
-        System.out.println("Kolon giriniz  :");
-        this.kolon = input.nextInt();
-        play();
-        PlayedPrint();
-        return 0;
+    void run() {
+        mineBoard();
+        minePrint();
+        gameBoard();
+        while (!isMine) {
+            if (!isWin()) {
+                System.out.println("Enter row number  :");
+                this.selectedRow = input.nextInt();
+                System.out.println("Enter column number  :");
+                this.selectedCol = input.nextInt();
+                if (this.selectedRow <= this.row && this.selectedCol <= this.col &&
+                        this.selectedRow >= 0 && this.selectedCol >= 0)
+                    play();
+                else {
+                    System.out.println("=======");
+                    System.out.println("Please enter row and column values correctly");
+                    System.out.println("=======");
+                }
+            } else
+                break;
+        }
     }
 
-    void MineBoard() {
+    // Creating random placed minefield
+    void mineBoard() {
         Random rand = new Random();
         int mine = (this.row * this.col) / 4;
         int a = 0;
-        //String[][] board2 = new String[this.row][this.col];
         while (a < mine) {
             int b = rand.nextInt(this.row);
             int c = rand.nextInt(this.col);
-            if (this.board2[b][c] == null) {
-                this.board2[b][c] = " * ";
+            if (this.mineBoard[b][c] == null) {
+                this.mineBoard[b][c] = " * ";
                 a++;
             }
         }
-        for (int i = 0; i < this.board2.length; i++)
-            for (int j = 0; j < this.board2[i].length; j++)
-                if (this.board2[i][j] == null)
-                    this.board2[i][j] = " - ";
+        for (int i = 0; i < this.mineBoard.length; i++)
+            for (int j = 0; j < this.mineBoard[i].length; j++)
+                if (this.mineBoard[i][j] == null)
+                    this.mineBoard[i][j] = " - ";
 
     }
 
-    void print() {
-        for (String[] row : this.board2) {
+    // Printing the Minefield
+    void minePrint() {
+        for (String[] row : this.mineBoard) {
             for (String col : row)
                 System.out.print(col);
             System.out.println();
         }
-        System.out.print("==============");
-
+        System.out.println("==============");
     }
 
-    String[][] play() {
-        int sum = 0, a = satir - 1, b = kolon - 1;
-        this.played = this.board2;
-        if (Objects.equals(this.played[satir][kolon], " - ")) {
-            if (satir != 0 && satir != this.row && kolon != 0 && kolon != this.col) {
-                for (int i = 0; i < 3; i++) {
-                    for (int j = 0; j < 3; j++)
-                        try {
-                            if (this.played[a][b++].equals(" * ")) {
-                                sum++;
-                            }
-                        } catch (Exception ignored){}
-                    a++;
+    // Checking nearby mine's and writing them to played index
+    // Checking whether player stepped onto mine or not
+    void play() {
+        int sum = 0, a = selectedRow - 1;
+        if (this.mineBoard[selectedRow][selectedCol].equals(" - ")) {
+            for (int i = 0; i < 3; i++) {
+                int b = selectedCol - 1;
+                for (int j = 0; j < 3; j++) {
+                    try {
+                        if (this.mineBoard[a][b++].equals(" * ")) {
+                            sum++;
+                        }
+                    } catch (Exception ignored) {
+                    }
                 }
-                this.played[satir][kolon] = Integer.toString(sum);
+                a++;
             }
-        } else {
-            System.out.println("You have lost");
+            this.gameBoard[selectedRow][selectedCol] = " " + sum + " ";
+            this.mineBoard[selectedRow][selectedCol] = " " + sum + " ";
+            GamePrint();
+        } else if (this.mineBoard[selectedRow][selectedCol].equals(" * ")) {
+            this.isMine = true;
+            System.out.println("=========================");
+            System.out.println("You have stepped on mine!");
+            System.out.println("Game Over!!");
+            System.out.println("=========================");
+            minePrint();
+        } else {   // (Integer.parseInt(this.mineBoard[selectedRow][selectedCol].trim()) <= this.mine) {
+            System.out.println("You have enter the same values");
+            System.out.println("Please choose another ones");
         }
-
-        return this.played;
     }
 
-    void PlayedPrint() {
-        for (String[] row : this.played) {
+    // Checking if the game being won
+    boolean isWin() {
+        for (String[] row : this.mineBoard) {
+            for (String col : row)
+                if (col.equals(" - ")) {
+                    return false;
+                }
+        }
+        System.out.println("You have won!! Congratulations!");
+        System.out.println("===============================");
+        minePrint();
+        return true;
+    }
+
+    // Creating player's game board
+    void gameBoard() {
+        for (int i = 0; i < this.gameBoard.length; i++)
+            for (int j = 0; j < this.gameBoard[i].length; j++)
+                if (this.gameBoard[i][j] == null)
+                    this.gameBoard[i][j] = " - ";
+    }
+
+    // Printing player's steps
+    void GamePrint() {
+        for (String[] row : this.gameBoard) {
             for (String col : row)
                 System.out.print(col);
             System.out.println();
         }
-        System.out.print("==============");
-
+        System.out.println("==============");
     }
 
 }
